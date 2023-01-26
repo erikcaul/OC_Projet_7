11395b08e99901ec1bdea662353aca506f663180
erikcaul <erikcaul@gmail.com>
2023-01-26 10:08:42 -0500

    remove old forcebrute.py

--- forcebrute.py
+++ forcebrute.py
@@ -31,112 +31,57 @@ with open('actions_list.csv') as csvfile:
 
 
 budget = 500
+nb_possibilities = 2**20 - 1
 
+def binary_convertion(possibility): 
+    binary_chain = format(possibility, "b")
+    inversed_chain = ''.join(reversed(binary_chain))
+    bit = enumerate(inversed_chain)
+    return bit
 
-""" for action in actions_list:
-    cost = 0
-    profit = 0
-    best_solution += 1
-    print(action["name"])
-    purchase_list = []
-    for i in range(len(actions_list)):
-        if action["name"] != actions_list[i]["name"]:
-            if cost < budget:
-                cost = cost + int(actions_list[i]['price'])
-                profit = profit + float(actions_list[i]['profit'])
-                purchase_list.append(actions_list[i]["name"])
-                if cost > budget:
-                    cost = cost - int(actions_list[i]['price'])
-                    profit = profit - float(actions_list[i]['profit']) 
-                if profit > best_profit:
-                    best_profit = profit
-                    best = best_solution
-    print("The purchase actions list is : " + str(purchase_list))  
-    print("The profit of the solution " + str(best_solution) + " is :" + str(profit))
-    print("The cost of this solution is :" + str(cost))             
-print("Best solution is the solution " + str(best) + " = " + str(best_profit))
- """
-# fonctions recursives
-def action_combinations(actions_list):
+def values_updated(index, cost, profit, purchase_list):
+    cost = cost + int(actions_list[index]['price'])
+    profit = profit + float(actions_list[index]['profit'])
+    purchase_list.append(actions_list[index]["name"])
+    return (cost, profit)
+
+def actions_combination(nb_possibilities, actions_list, budget):
     best_profit = 0
     best_solution = 0
+    best_purchase_list_cost = 0
     best = 0
-    for action_number in range(len(actions_list)):
+    best_purchase_list = []
+    for i in range(nb_possibilities):
         cost = 0
         profit = 0
         best_solution = best_solution + 1
-        purchase_list = []
-        cost = cost + int(actions_list[action_number]['price'])
-        profit = profit + float(actions_list[action_number]['profit'])
-        purchase_list.append(actions_list[action_number]["name"])
-        if profit > best_profit:
-            best_profit = profit
-            best = best_solution
-        i = action_number + 1
-        for action_number in range(i, len(actions_list[i:])):
-            if cost < budget:
-                cost = cost + int(actions_list[action_number]['price'])
-                profit = profit + float(actions_list[action_number]['profit'])
-                purchase_list.append(actions_list[action_number]["name"])
-                if cost > budget:
-                    cost = cost - int(actions_list[action_number]['price'])
-                    profit = profit - float(actions_list[action_number]['profit']) 
+        purchase_list = []       
+        bit = binary_convertion(i)
+        for index, value in bit:
+            if value == chr(ord('1')) and (cost + int(actions_list[index]['price'])) < budget:
+                value_updated = values_updated(index, cost, profit, purchase_list)
+                cost = value_updated[0]
+                profit = value_updated[1]
                 if profit > best_profit:
                     best_profit = profit
                     best = best_solution
-        actions_rest_list = actions_list.pop(action_number)
-        if len(actions_rest_list) == 0:
-            return 0
-        else:
-            action_combinations(actions_rest_list)
-        """ i = action_number + 1
-        for action_number in range(i, len(actions_rest_list[i:])):
-            if cost < budget:
-                cost = cost + int(actions_list[action_number]['price'])
-                profit = profit + float(actions_list[action_number]['profit'])
-                purchase_list.append(actions_list[action_number]["name"])
-                if cost > budget:
-                    cost = cost - int(actions_list[action_number]['price'])
-                    profit = profit - float(actions_list[action_number]['profit']) 
-                if profit > best_profit:
-                    best_profit = profit
-                    best = best_solution """
-        print("The purchase actions list is : " + str(purchase_list))  
-        print("The profit of the solution " + str(best_solution) + " is :" + str(profit))
-        print("The cost of this solution is :" + str(cost))             
-    print("Best solution is the solution " + str(best) + " = " + str(best_profit))
-            
-        
-action_combinations(actions_list)       
-
-# Pour une action donnée, je dois faire une liste de toutes les possibilités d'achats avec budget < 500
-    # une liste du numéro action à max
-    # une liste pour chaque autres possibilités
-         #     
-# répéter pour toutes les actions  
-# ------------
-# utiliser la forme binaire (0,1) : chaque élément ne peut prendre que 2 valeurs : une liste avec chaque élément, s'il est à 0 je l'utilise pas si à 1 je l'utilise
-# liste de 4 éléments : comme si j'avais un nombre avec une représentation à 4 bit
-# ex: 15 = 1111
-# avec 4 éléments = un max de 15 combinaisons
-# je prend le nombre d'éléments dans la liste
-# for (2^nombre) - 1 : nombre de possibilités que j'ai au total = boucle ---> for i in range(2^20):
-    # convertir en chaine binaire (un chiffre en sa représentation binaire) ---> convertir i en  chaine_binaire
-    # for bit in chaine_binaire -(str)-: boucle
-        # if bit -(chr)- == chr(ord('1')): 
-            # prends les valeurs
-        # enumerate bit in chaine_binaire: (lire la doc)
-        # if bit == chr("1"):
-        # index variable pour prendre index de la bonne action
-     
-# type chr, utiliser l'objet char pour la comparaison
-# string une liste objet char
-# doit être comparé à un autre char
+                    best_purchase_list = purchase_list
+                    best_purchase_list_cost = cost         
+    return (best, best_profit, best_purchase_list, best_purchase_list_cost)   
+    
+    
+def display_result(best, best_profit, best_purchase_list, best_purchase_list_cost):     
+    print("----------------------------------------------------------------------------------------------------------")
+    print("----------------------------------------------------------------------------------------------------------")
+    print("Best solution is the solution " + str(best))
+    print("The best profit = " + str(best_profit))
+    print("The best purchase list is : " + str(best_purchase_list))
+    print("The cost of the best purchase list is :" + str(best_purchase_list_cost))        
 
 
-# essayer toutes les possibilit/s et essayer la meilleure
-# essayer avec action 1 et toutes les autres et calculer le benefice (jusqu'a 500)
-# voir celui qui donne le max de benefice
-# voir modele algo sur internet
-# calculer le b/n/fice et le cout pour chaque combinaison
-# garder le meilleur gain pour un budget <= 500
+result = actions_combination(nb_possibilities, actions_list, budget)
+best = result[0]
+best_profit = result[1]
+best_purchase_list = result[2]
+best_purchase_list_cost = result[3]
+display_result(best, best_profit, best_purchase_list, best_purchase_list_cost)
