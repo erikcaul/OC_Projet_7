0785e7261ff0834191b452f2f22d5d64dc687a1f
erikcaul <erikcaul@gmail.com>
2023-02-13 10:11:29 -0500

    after 13 feb mentorat session

--- optimized.py
+++ optimized.py
@@ -13,7 +13,6 @@ with open('dataset2.csv') as csvfile:
     dataset2 = list(reader)
 
 budget = 500
-purchase_list = []
 
 
 def sort_data(data_list):
@@ -22,34 +21,41 @@ def sort_data(data_list):
 
 
 def optimized_algo(sorted_data):
-    profit = 0
-    cost = 0
+    purchase_list = []
+    profit = 0.0
+    cost = 0.0
     for i in range(len(sorted_data)):
-        if cost < budget:
+        if float(sorted_data[i]["price"]) <= 0.0:
+            continue
+        if (cost + float(sorted_data[i]['price'])) <= budget:
             cost = cost + float(sorted_data[i]['price'])
             profit = profit + float(sorted_data[i]['profit'])
-            purchase_list.append(sorted_data[i]["name"])
-            if cost > budget:
-                cost = cost - float(sorted_data[i]['price'])
-                profit = profit - float(sorted_data[i]['profit'])
-    return {"cost": cost, "profit": profit}
+            purchase_list.append(sorted_data[i])
+    return {"cost": cost, "profit": profit, "purchase_list": purchase_list}
 
 
-def display_optimized_result(cost, profit):
-    """ print("The purchase actions list is : " + str(purchase_list))   """
+def display_optimized_result(cost, profit, purchase_list):
+    print("The purchase actions list is : " + str(purchase_list))
     print("The cost of this solution is :" + str(cost))
     print("The profit of this solution is :" + str(profit))
 
 
+# optimized alog with actions_list data
 sorted_actions_list = sort_data(actions_list)
 result_actions_list = optimized_algo(sorted_actions_list)
 display_optimized_result(result_actions_list["cost"],
-                         result_actions_list["profit"])
+                         result_actions_list["profit"],
+                         result_actions_list["purchase_list"])
 
+
+# optimized alog with dataset1
 sorted_dataset1 = sort_data(dataset1)
 result_dataset1 = optimized_algo(sorted_dataset1)
-display_optimized_result(result_dataset1["cost"], result_dataset1["profit"])
+display_optimized_result(result_dataset1["cost"], result_dataset1["profit"],
+                         result_dataset1["purchase_list"])
 
+# optimized alog with dataset2
 sorted_dataset2 = sort_data(dataset2)
 result_dataset2 = optimized_algo(sorted_dataset2)
-display_optimized_result(result_dataset2["cost"], result_dataset2["profit"])
+display_optimized_result(result_dataset2["cost"], result_dataset2["profit"],
+                         result_dataset2["purchase_list"])
