4e39712c56199d560f3f4685be2043427827cc8c
erikcaul <erikcaul@gmail.com>
2023-02-13 09:15:48 -0500

    add flake8 and run it

--- forcebrute.py
+++ forcebrute.py
@@ -3,27 +3,27 @@ import csv
 with open('actions_list.csv', 'w', newline='') as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    filewriter.writerow(['name','price','profit'])
-    filewriter.writerow(['action_1',20,0.05])
-    filewriter.writerow(['action_2',30,0.1])
-    filewriter.writerow(['action_3',50,0.15])
-    filewriter.writerow(['action_4',70,0.20])
-    filewriter.writerow(['action_5',60,0.17])
-    filewriter.writerow(['action_6',80,0.25])
-    filewriter.writerow(['action_7',22,0.07])
-    filewriter.writerow(['action_8',26,0.11])
-    filewriter.writerow(['action_9',48,0.13])
-    filewriter.writerow(['action_10',34,0.27])
-    filewriter.writerow(['action_11',42,0.17])
-    filewriter.writerow(['action_12',110,0.09])
-    filewriter.writerow(['action_13',38,0.23])
-    filewriter.writerow(['action_14',14,0.01])
-    filewriter.writerow(['action_15',18,0.03])
-    filewriter.writerow(['action_16',8,0.08])
-    filewriter.writerow(['action_17',4,0.12])
-    filewriter.writerow(['action_18',10,0.14])
-    filewriter.writerow(['action_19',24,0.21])
-    filewriter.writerow(['action_20',114,0.18])
+    filewriter.writerow(['name', 'price', 'profit'])
+    filewriter.writerow(['action_1', 20, 0.05])
+    filewriter.writerow(['action_2', 30, 0.1])
+    filewriter.writerow(['action_3', 50, 0.15])
+    filewriter.writerow(['action_4', 70, 0.20])
+    filewriter.writerow(['action_5', 60, 0.17])
+    filewriter.writerow(['action_6', 80, 0.25])
+    filewriter.writerow(['action_7', 22, 0.07])
+    filewriter.writerow(['action_8', 26, 0.11])
+    filewriter.writerow(['action_9', 48, 0.13])
+    filewriter.writerow(['action_10', 34, 0.27])
+    filewriter.writerow(['action_11', 42, 0.17])
+    filewriter.writerow(['action_12', 110, 0.09])
+    filewriter.writerow(['action_13', 38, 0.23])
+    filewriter.writerow(['action_14', 14, 0.01])
+    filewriter.writerow(['action_15', 18, 0.03])
+    filewriter.writerow(['action_16', 8, 0.08])
+    filewriter.writerow(['action_17', 4, 0.12])
+    filewriter.writerow(['action_18', 10, 0.14])
+    filewriter.writerow(['action_19', 24, 0.21])
+    filewriter.writerow(['action_20', 114, 0.18])
 
 with open('actions_list.csv') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -33,17 +33,20 @@ with open('actions_list.csv') as csvfile:
 budget = 500
 nb_possibilities = 2**(len(actions_list)-1) - 1
 
-def binary_convertion(possibility): 
+
+def binary_convertion(possibility):
     binary_chain = format(possibility, "b")
     inversed_chain = ''.join(reversed(binary_chain))
     bit = enumerate(inversed_chain)
     return bit
 
+
 def values_updated(index, cost, profit, purchase_list):
     cost = cost + int(actions_list[index]['price'])
     profit = profit + float(actions_list[index]['profit'])
     purchase_list.append(actions_list[index]["name"])
-    return {"cost":cost, "profit":profit}
+    return {"cost": cost, "profit": profit}
+
 
 def actions_combination(nb_possibilities, actions_list, budget):
     best_profit = 0
@@ -55,30 +58,35 @@ def actions_combination(nb_possibilities, actions_list, budget):
         cost = 0
         profit = 0
         best_solution = best_solution + 1
-        purchase_list = []       
+        purchase_list = []
         bit = binary_convertion(possibility)
         for index, value in bit:
             if value == chr(ord('1')):
                 if (cost + int(actions_list[index]['price'])) > budget:
                     break
-                value_updated = values_updated(index, cost, profit, purchase_list)
+                value_updated = values_updated(index, cost,
+                                               profit, purchase_list)
                 cost = value_updated["cost"]
                 profit = value_updated["profit"]
                 if profit > best_profit:
                     best_profit = profit
                     best = best_solution
                     best_purchase_list = purchase_list
-                    best_purchase_list_cost = cost         
-    return {"best":best, "best_profit": best_profit, "best_purchase_list": best_purchase_list, "best_purchase_list_cost": best_purchase_list_cost}   
-    
-    
-def display_result(best, best_profit, best_purchase_list, best_purchase_list_cost):     
-    print("----------------------------------------------------------------------------------------------------------")
-    print("----------------------------------------------------------------------------------------------------------")
+                    best_purchase_list_cost = cost
+    return {"best": best, "best_profit": best_profit,
+            "best_purchase_list": best_purchase_list,
+            "best_purchase_list_cost": best_purchase_list_cost}
+
+
+def display_result(best, best_profit, best_purchase_list,
+                   best_purchase_list_cost):
+    print("-------------------------------------------------------")
+    print("-------------------------------------------------------")
     print("Best solution is the solution " + str(best))
     print("The best profit = " + str(best_profit))
     print("The best purchase list is : " + str(best_purchase_list))
-    print("The cost of the best purchase list is :" + str(best_purchase_list_cost))        
+    print("The cost of the best purchase list is :" +
+          str(best_purchase_list_cost))
 
 
 result = actions_combination(nb_possibilities, actions_list, budget)
--- optimized.py
+++ optimized.py
@@ -15,9 +15,11 @@ with open('dataset2.csv') as csvfile:
 budget = 500
 purchase_list = []
 
+
 def sort_data(data_list):
-    sorted_data = sorted(data_list, key=lambda x:x['profit'], reverse=True)
+    sorted_data = sorted(data_list, key=lambda x: x['profit'], reverse=True)
     return sorted_data
+
 
 def optimized_algo(sorted_data):
     profit = 0
@@ -30,16 +32,19 @@ def optimized_algo(sorted_data):
             if cost > budget:
                 cost = cost - float(sorted_data[i]['price'])
                 profit = profit - float(sorted_data[i]['profit'])
-    return {"cost":cost, "profit":profit} 
+    return {"cost": cost, "profit": profit}
+
 
 def display_optimized_result(cost, profit):
     """ print("The purchase actions list is : " + str(purchase_list))   """
     print("The cost of this solution is :" + str(cost))
-    print("The profit of this solution is :" + str(profit))           
-    
+    print("The profit of this solution is :" + str(profit))
+
+
 sorted_actions_list = sort_data(actions_list)
 result_actions_list = optimized_algo(sorted_actions_list)
-display_optimized_result(result_actions_list["cost"], result_actions_list["profit"])
+display_optimized_result(result_actions_list["cost"], 
+                         result_actions_list["profit"])
 
 sorted_dataset1 = sort_data(dataset1)
 result_dataset1 = optimized_algo(sorted_dataset1)
@@ -48,4 +53,3 @@ display_optimized_result(result_dataset1["cost"], result_dataset1["profit"])
 sorted_dataset2 = sort_data(dataset2)
 result_dataset2 = optimized_algo(sorted_dataset2)
 display_optimized_result(result_dataset2["cost"], result_dataset2["profit"])
-
